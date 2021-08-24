bae82b72a51089614f8123edcfb1e67580914f59
Agares404 <89093786+Agares404@users.noreply.github.com>
2021-08-23 20:12:35 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
+
+print("Costo mensual del teléfono")
+men = int(input("Dame el número de mensajes: "))
+me = float(input("Dame el numero de megas: "))
+min = int(input("Dame el número de minutos: "))
+tot = (men+me+min)*.8
+print("El costo mensual es: ",tot)
+
     #Leer los datos
     pass
 
