cf06dd91b96d158aaeffb5dd1b0f49906ef74275
Agares404 <89093786+Agares404@users.noreply.github.com>
2021-08-23 19:59:55 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
+print("Centimetros recorridos por una caracol en 'x' minutos")
+cen=float(input("Introduzca la cantidad de minutos: "))
+tot=cen*.342
+print("En", cen, "minutos el caracol recorre", tot, "centimetros")
     pass
 
 if __name__ == '__main__':
